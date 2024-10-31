fdea2bfeb08045dc16a61ec964423b3a54cb58dd
Lucas Toledo <46853656+lucastol3do@users.noreply.github.com>
2024-10-31 13:08:40 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import CourseDetailsScreen from './screens/CourseDetailsScreen';
+import SearchScreen from './screens/SearchScreen';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="CourseDetails" component={CourseDetailsScreen} options={{ title: 'Detalhes do Curso' }} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Search') {
+              iconName = 'search';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Search" component={SearchScreen} options={{ title: 'Pesquisa' }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
